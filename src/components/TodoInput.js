561a032a7d1f198298b1fd89e3a5ad878ea112ae
anit adhikari <anitjee@gmail.com>
2020-01-04 15:45:24 -0700

    First Commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TodoInput
---
+++ src/components/TodoList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+
+export class TodoList extends Component {
+  render() {
+    return (
+      <div>
+          <section>
+              <h2>Hello from todo list</h2>
+          </section>
+          <TodoItem/>
+      </div>
+    )
+  }
+}
+
+export default TodoList
